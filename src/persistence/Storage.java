6df7981d4e56cf155c3022acdf016047212d13ef
Scholzova Lucie <st64176@upce.cz>
2024-01-17 15:35:39 +0100

    small refactor in SStorage.java

--- src/persistence/Storage.java
+++ src/persistence/Storage.java
@@ -13,8 +13,6 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -30,17 +28,11 @@ public class Storage {
         classfolder.mkdir();
         
         File objectfile = new File(classfolder + "/" + object.hashCode());
-        ObjectOutputStream oos = null;
+        ObjectOutputStream objectOutputStream = null;
         try {
-            oos = new ObjectOutputStream(new FileOutputStream(objectfile));
-        } catch (IOException ex) {
-        }
-        try {
-            oos.writeObject(object);
-        } catch (IOException ex) {
-        }
-        try {
-            oos.close();
+            objectOutputStream = new ObjectOutputStream(new FileOutputStream(objectfile));
+            objectOutputStream.writeObject(object);
+            objectOutputStream.close();
         } catch (IOException ex) {
         }
     }
@@ -48,34 +40,28 @@ public class Storage {
     public void unstore(Object object) {
         File storage = new File("./storage");
         
-        File classfolder = new File(storage.getAbsolutePath() + "/" + object.getClass().getName());
+        File classFolder = new File(storage.getAbsolutePath() + "/" + object.getClass().getName());
         
-        File objectfile = new File(classfolder + "/" + object.hashCode());
+        File objectfile = new File(classFolder + "/" + object.hashCode());
         objectfile.delete();
     }
     
     public <T> List<T> load(Class<T> clazz) {
         File storage = new File("./storage");
-        File classfolder = new File(storage.getAbsolutePath() + "/" + clazz.getName());
+        File classFolder = new File(storage.getAbsolutePath() + "/" + clazz.getName());
         
         List<T> list = new ArrayList<>();
-        File[] files = classfolder.listFiles();
+        File[] files = classFolder.listFiles();
         
         for (File file : files) {
             ObjectInputStream ois = null;
             try {
                 ois = new ObjectInputStream(new FileInputStream(file));
-            } catch (IOException ex) {
-            }
-            try {
                 list.add((T) ois.readObject());
-            } catch (IOException ex) {
-            } catch (ClassNotFoundException ex) {
-            }
-            try {
                 ois.close();
-            } catch (IOException ex) {
+            } catch (IOException | ClassNotFoundException ex) {
             }
+
         }
         
         return list;
